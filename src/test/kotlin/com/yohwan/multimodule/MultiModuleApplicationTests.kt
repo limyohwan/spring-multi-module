24a3ea7b91d48505a83e31954136f69fdb9f98b7
Yohwan Lim <dyghks7102@naver.com>
2023-10-09 22:24:15 +0900

    first commit

---
+++ src/test/kotlin/com/yohwan/multimodule/MultiModuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yohwan.multimodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiModuleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
