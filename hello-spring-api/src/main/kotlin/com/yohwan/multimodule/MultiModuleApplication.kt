be38801721af3e1ac9bc55716d60f40fc9c09054
Yohwan Lim <dyghks7102@naver.com>
2023-10-13 23:12:50 +0900

    feat. @ComponenetScan 대신 @SpringBootApplication(scanBasePackages = ["com.yohwan"])를 사용하여도 가능

--- hello-spring-api/src/main/kotlin/com/yohwan/multimodule/MultiModuleApplication.kt
+++ hello-spring-api/src/main/kotlin/com/yohwan/multimodule/MultiModuleApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
-@ComponentScan(basePackages = ["com.yohwan"])
-@SpringBootApplication
+//@ComponentScan(basePackages = ["com.yohwan"])
+@SpringBootApplication(scanBasePackages = ["com.yohwan"])
 class MultiModuleApplication
 
 fun main(args: Array<String>) {
